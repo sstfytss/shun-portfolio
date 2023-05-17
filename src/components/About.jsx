ff4a64792a30a90811d0bf89f259b4dbebd8dd6f
Shun Sakai <shunsakai1217@gmail.com>
2023-05-17 19:35:12 -0400

    draft

---
+++ src/components/About.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b
+    from-gray-800 to-black text-white'>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center
+        w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline
+                border-b-4 border-gray-500'>
+                    About
+                </p>
+            </div>
+
+            <p className='text-xl mt-20'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod aliquam ea excepturi, repellendus odit quaerat quibusdam hic sed pariatur deserunt qui quidem ut dicta, fuga nulla vitae repellat explicabo omnis eveniet soluta asperiores. Qui quos magni nulla molestias! Maiores, vitae nulla ex tempore atque explicabo porro veniam. Quos, totam illo.
+            </p>
+
+            <br />
+
+            <p className='text-xl'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis tenetur odio doloribus fugit tempore impedit dicta exercitationem quod, porro eos nulla quibusdam, ratione laborum facilis aliquid suscipit rem natus architecto reiciendis eum esse, sit voluptatem magni? Ipsa, ab ut! Minima, sed accusamus. Perspiciatis, voluptas doloribus necessitatibus labore temporibus commodi delectus.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
