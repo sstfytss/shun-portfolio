0e06aeff5da2aba88b007044a6dba27214b7e5d8
Shun Sakai <shunsakai1217@gmail.com>
2023-05-18 11:57:20 -0400

    changed languages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./components/Home";
 import SocialLinks from "./components/SocialLinks";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
-import Experience from "./components/Experience";
+import Technologies from "./components/Experience";
 import Contact from "./components/Contact";
 
 
@@ -13,8 +13,8 @@ function App() {
       <NavBar />
       <Home />
       <About />
+      <Technologies />
       <Portfolio />
-      <Experience />
       <Contact />
 
       <SocialLinks />
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,80 +1,79 @@
 import React from 'react'
 
-import html from "../assets/html.png";
-import css from "../assets/css.png";
-import javascript from "../assets/javascript.png";
-import reactImage from "../assets/react.png";
-import nextjs from "../assets/nextjs.png";
-import graphql from "../assets/graphql.png";
-import github from "../assets/github.png";
-import tailwind from "../assets/tailwind.png";
+import python from "../assets/python.png";
+import swift from "../assets/swift.png";
+import java from "../assets/java.png";
+import cplusplus from "../assets/c++.png";
+import postgres from "../assets/postgres.png";
+import firebase from "../assets/firebase.png";
+import plotly from "../assets/plotly.png";
+import git from "../assets/git.png";
 
 const Experience = () => {
 
     const techs = [
         {
             id: 1,
-            src: html,
-            title: 'HTML',
-            style: 'shadow-orange-500'
-        },
-        {
-            id: 2,
-            src: css,
-            title: 'CSS',
+            src: java,
+            title: 'Java',
             style: 'shadow-blue-500'
         },
         {
-            id: 3,
-            src: javascript,
-            title: 'JavaScript',
+            id: 2,
+            src: python,
+            title: 'Python',
             style: 'shadow-yellow-500'
         },
         {
+            id: 3,
+            src: swift,
+            title: 'Swift',
+            style: 'shadow-orange-500'
+        },
+
+        {
             id: 4,
-            src: reactImage,
-            title: 'React',
-            style: 'shadow-blue-600'
+            src: cplusplus,
+            title: 'C++',
+            style: 'shadow-blue-800'
         },
         {
             id: 5,
-            src: tailwind,
-            title: 'Tailwind',
-            style: 'shadow-sky-400'
+            src: firebase,
+            title: 'Firebase',
+            style: 'shadow-orange-400'
         },
         {
             id: 6,
-            src: nextjs,
-            title: 'Next JS',
-            style: 'shadow-white'
+            src: postgres,
+            title: 'PostgreSQL',
+            style: 'shadow-sky-400'
         },
         {
             id: 7,
-            src: graphql,
-            title: 'GraphQL',
-            style: 'shadow-pink-400'
+            src: git,
+            title: 'Git',
+            style: 'shadow-orange-600'
         },
         {
             id: 8,
-            src: github,
-            title: 'GitHub',
-            style: 'shadow-gray-400'
+            src: plotly,
+            title: 'Plotly/Dash',
+            style: 'shadow-gray-800'
         },
     ]
 
   return (
-    <div name='experience' className='bg-gradient-to-b from-gray-800 to-black
+    <div name='experience' className='bg-white
     w-full h-screen'>
-        <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
+        <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-black'>
             <div>
                 <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>
-                    Experience
+                    LANGUAGES/LIBRARIES/TECH.
                 </p>
-                <p className='py-6'>These are the technlogies I've worked with</p>
             </div>
 
             <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
-
 
                 {
                     techs.map(({id, src, title, style}) => (
