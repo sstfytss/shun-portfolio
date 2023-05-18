e54c625869689d421b96bb5ad1865a1b4460a32c
Shun Sakai <shunsakai1217@gmail.com>
2023-05-18 11:58:17 -0400

    removed border

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -68,7 +68,7 @@ const Experience = () => {
     w-full h-screen'>
         <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-black'>
             <div>
-                <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>
+                <p className='text-4xl font-bold p-2'>
                     LANGUAGES/LIBRARIES/TECH.
                 </p>
             </div>
