beb95ee4304c5fc53ef3173d1ed59ca13cf0bf34
Shun Sakai <shunsakai1217@gmail.com>
2023-05-18 12:02:35 -0400

    rem border

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -39,8 +39,7 @@ const Portfolio = () => {
     <div name="portfolio" className='bg--white text-black md:h-screen'>
      <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
-            <p className='text-4xl font-bold inline border-b-4
-            border-gray-500'>
+            <p className='text-4xl font-bold'>
                 PROJECTS
             </p>
             <p className='py-6'>
