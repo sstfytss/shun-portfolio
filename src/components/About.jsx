db9750781cf95277af3449c348630d8c484e5418
Shun Sakai <shunsakai1217@gmail.com>
2023-05-17 22:09:46 -0400

    added photo to about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ProfileImage from '../assets/heroImage.png'
 
 const About = () => {
   return (
@@ -13,15 +14,20 @@ const About = () => {
                 </p>
             </div>
 
-            <p className='text-xl mt-20'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod aliquam ea excepturi, repellendus odit quaerat quibusdam hic sed pariatur deserunt qui quidem ut dicta, fuga nulla vitae repellat explicabo omnis eveniet soluta asperiores. Qui quos magni nulla molestias! Maiores, vitae nulla ex tempore atque explicabo porro veniam. Quos, totam illo.
-            </p>
+            <div className='flex items-center justify-center h-full space-x-4 md:flex-row'>
+                <div className='min-h-full w-1/2'>
+                    <h1 className='text-xl sm:text-5xl pb-1 font-bold text-white'>Shun Sakai</h1>
+                    <h2 className='text-base sm:text-4xl pb-4 font-bold text-white'>酒井 駿</h2>
+                    <h2 className='text-xl sm:text-2xl pb-8 text-white'>Duke University</h2>
+                    <p className='text-xl'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod aliquam ea excepturi, repellendus odit quaerat quibusdam hic sed pariatur deserunt qui quidem ut dicta, fuga nulla vitae repellat explicabo omnis eveniet soluta asperiores. Qui quos magni nulla molestias! Maiores, vitae nulla ex tempore atque explicabo porro veniam. Quos, totam illo.
+                    </p>
+                </div>
 
-            <br />
-
-            <p className='text-xl'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis tenetur odio doloribus fugit tempore impedit dicta exercitationem quod, porro eos nulla quibusdam, ratione laborum facilis aliquid suscipit rem natus architecto reiciendis eum esse, sit voluptatem magni? Ipsa, ab ut! Minima, sed accusamus. Perspiciatis, voluptas doloribus necessitatibus labore temporibus commodi delectus.
-            </p>
+                <div className='min-h-full w-1/2'>
+                    <img src={ProfileImage} alt="my profile" className='rounded-2xl mx-auto w-2/3 md:w-full' />
+                </div>
+            </div>
         </div>
     </div>
   )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -32,7 +32,7 @@ const NavBar = () => {
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
       <div>
-        <h1 className="text-5xl font-signature ml-2">Shun</h1>
+        <h1 className="text-2xl pl-4">SHUN SAKAI</h1>
       </div>
 
       <ul className='hidden md:flex'>
