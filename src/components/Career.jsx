89955b98f6cbbd3af82c648a464cadb899201d11
Shun Sakai <shunsakai1217@gmail.com>
2023-05-19 23:51:34 -0400

    added experience

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -2,7 +2,36 @@ import React from 'react'
 
 const Career = () => {
   return (
-    <div>Career</div>
+    <div name="career" className='bg--white text-black md:h-screen'>
+     <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+            <p className='text-4xl font-bold pb-8'>
+                EXPERIENCE
+            </p>
+            <p className='text-2xl font-bold'>
+                Ametek - Zygo Corportation
+            </p>
+            <p className='font-bold pb-8'>Software Engineering Intern</p>
+
+            <p className='text-2xl font-bold'>
+                Humans and Autonomy Lab, Duke University
+            </p>
+            <p className='font-bold pb-8'>Software Engineer/Research Assistant</p>
+
+            <p className='text-2xl font-bold'>
+                Big Ideas Lab, Duke University
+            </p>
+            <p className='font-bold pb-8'>Software Engineer/Research Assistant</p>
+
+            <p className='text-2xl font-bold'>
+                Mitou Program of the Japanese Ministry of Economy, Trade and Industry
+            </p>
+            <p className='font-bold pb-8'>Mitou Program Developer</p>
+        </div>
+
+     </div>
+
+    </div>
   )
 }
 
